855fe7fb63467612e1e59feffb91aeb231e94c3b
thiagosalome <thiagosalome@gmail.com>
2018-03-21 02:10:43 -0300

    Consertando alguns itens de estilização

--- system/app/views/nutricionista/login.php
+++ system/app/views/nutricionista/login.php
@@ -1,53 +1,67 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+    <!-- meta tags -->
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- link css -->
     <link rel="stylesheet" href="/nutrion/system/app/public/css/style.css">
+    
+    <!-- google fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
+
+    <!-- jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- site title -->
     <title>Login</title>
 </head>
 <body>
     <main class="main">
         <header class="main-header">
-            <img src="/nutrion/system/app/public/images/logo.png" alt="" class="header-logo">
+            <img src="/nutrion/system/app/public/images/logo.png" alt="" title="" class="header-logo">
         </header>
         <div class="main-block">
-            
             <div class="block-description">
                 <img src="/nutrion/system/app/public/images/logo_mini.png" alt="" class="description-logo">
                 <p class="description-project">NutriOn é um sistema que busca ajudar os nutricionistas a gerenciarem a alimentação dos seus pacientes</p>
-                <button name="description-button" class="description-button">CADASTRAR</button>
+                <button name="description-button" class="description-button">Cadastrar</button>
             </div>
-
-            <div class="block-form">
-                <h1 class="logh1">Login</h1>
-                <form method="POST" action="/nutrion/system/nutricionista/logar" class="form-login">
-                    <label>Email</label>
-                    <input type="text" name="email" id="email"required><br>
-                    <label>Senha</label>
-                    <input type="password" name="senha" id="senha"><br>
-                    <input type="submit" value="LOGIN" class="login" name="login"required>
+            <div class="block-form block-form-log">
+                <h1 class="form-title">Login</h1>
+                <form method="POST" action="/nutrion/system/nutricionista/logar" class="form-content">
+                    <span class="form-field">
+                        <label>Email</label>
+                        <input type="text" name="email" id="email" required><br>
+                    </span>
+                    <span class="form-field">
+                        <label>Senha</label>
+                        <input type="password" name="senha" id="senha" required><br>
+                    </span>
+                    <input type="submit" value="login" name="login">
                 </form>
-                <a href="" class="esqueceu">Esqueceu a senha? </a>
-                
+                <a href="" class="form-forgot">Esqueceu a senha?</a>
             </div>
-
-            <div class="block-cad">
-                <h1 class="cadh1">Cadastro</h1>
-                
-                <form method="POST" action="/nutrion/system/nutricionista/logar" class="form-cad">
-                    <label>Nome</label>
-                    <input type="text" name="nome" id="nome"required><br>
-                    <label>Email</label>
-                    <input type="text" name="email" id="email"required><br>
-                    <label>Senha:</label>
-                    <input type="password" name="senha" id="senha"><br>
-                    <input type="submit" value="CADASTRAR" class="cadastrar" name="cadastrar"required>
+            <div class="block-form block-form-cad">
+                <h1 class="form-title">Cadastro</h1>
+                <form method="POST" action="/nutrion/system/nutricionista/logar" class="form-content">
+                    <span class="form-field">
+                        <label>Nome</label>
+                        <input type="text" name="nome" id="nome" required>
+                    </span>
+                    <span class="form-field">
+                        <label>Email</label>
+                        <input type="text" name="email" id="email" required>
+                    </span>
+                    <span class="form-field">
+                        <label>Senha:</label>
+                        <input type="password" name="senha" id="senha" required>
+                    </span>
+                    <input type="submit" value="Cadastrar" class="form-button" name="cadastrar">
                 </form>
             </div>
-
         </div>
     </main>
 </body>
