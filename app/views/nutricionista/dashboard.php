d6fda125db1c0dbe1d9196f4041d5deeaa2a6fa6
Thiago Goncalves Salome <1001479@sga.pucminas.br>
2018-04-10 21:50:47 -0300

    Separando menu do dashboard

--- app/views/nutricionista/dashboard.php
+++ app/views/nutricionista/dashboard.php
@@ -33,75 +33,7 @@
 <body>
     <main class = "main">
         <section class="column-menu">
-            <aside class = "aside-lateral">
-                <header class="header-aside">
-                    <img src="<?php echo HOME_URI; ?>app/public/images/dashboard/logo_menu.png" alt="" title="" class="header-logo">
-                </header>
-                <nav class="nav-menu">
-                    <ul>
-                        <li>
-                            <img src="<?php echo HOME_URI; ?>app/public/images/dashboard/patient_icon.png" alt="" class="icon-list">
-                            <p class="li-item">Pacientes</p>
-                            <img src="<?php echo HOME_URI; ?>app/public/images/dashboard/seta_icon.png" alt="" class="icon-seta">
-                            <ul>
-                                <li>
-                                    <a href="#" title="Item">
-                                        <img src="<?php echo HOME_URI; ?>app/public/images/dashboard/view_icon.png" alt="" title="" class="icon-view">
-                                        <p class="li-subitem">Consultar Pacientes</p>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#" title="Item">
-                                        <img src="<?php echo HOME_URI; ?>app/public/images/dashboard/add_icon.png" alt="" title="" class="icon-add">
-                                        <p class="li-subitem">Adicionar Paciente</p>
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <img src="<?php echo HOME_URI; ?>app/public/images/dashboard/aliments_icon.png" alt="" class="icon-list">
-                            <p class="li-item">Alimentos</p>
-                            <img src="<?php echo HOME_URI; ?>app/public/images/dashboard/seta_icon.png" alt="" class="icon-seta">
-                            <ul>
-                                <li>
-                                    <a href="#" title="Item">
-                                        <img src="<?php echo HOME_URI; ?>app/public/images/dashboard/view_icon.png" alt="" title="" class="icon-view">
-                                        <p class="li-subitem">Consultar Alimentos</p>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#" title="Item">
-                                        <img src="<?php echo HOME_URI; ?>app/public/images/dashboard/add_icon.png" alt="" title="" class="icon-add">
-                                        <p class="li-subitem">Adicionar Alimentos</p>
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <img src="<?php echo HOME_URI; ?>app/public/images/dashboard/menu_icon.png" alt="" class="icon-list">
-                            <p class="li-item">Dieta</p>
-                            <img src="<?php echo HOME_URI; ?>app/public/images/dashboard/seta_icon.png" alt="" class="icon-seta">
-                            <ul>
-                                <li>
-                                    <a href="#" title="Item">
-                                        <img src="<?php echo HOME_URI; ?>app/public/images/dashboard/view_icon.png" alt="" title="" class="icon-view">
-                                        <p class="li-subitem">Consultar Dieta</p>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#" title="Item">
-                                        <img src="<?php echo HOME_URI; ?>app/public/images/dashboard/add_icon.png" alt="" title="" class="icon-add">
-                                        <p class="li-subitem">Adicionar Dieta</p>
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </nav>
-                <footer class="rodape-menu">
-                    <p>&copy; Copyright - Todos os direitos reservados</p>
-                </footer>
-            </aside>
+            <?php include "objects/menu.php"; ?>
         </section>
         <section class="column-page">
             <header class="header-superior">
