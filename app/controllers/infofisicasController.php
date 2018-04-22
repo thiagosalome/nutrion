ab13ab83a427d99060047c41bce7a7297675aa02
Lorran Martins <33589594+lorransm@users.noreply.github.com>
2018-04-22 14:37:56 -0300

    Criando estrutura InfoFisicas. ORM e Vo Implementadas

---
+++ app/controllers/infofisicasController.php
@@ -0,0 +1,16 @@
+<?php
+require "app/models/infofisicas/infofisicasDAO.php";
+require "app/models/infofisicas/infofisicasVo.php";
+require "app/models/infofisicas/infofisicasModel.php";
+
+class infofisicasController{
+    public function create() {
+        $infofisicasModel = new infofisicasModel();
+        $infofisicaVo = new infofisicaVo();
+
+        
+    }
+
+
+
+?>
