86f265281229c04acc0fbf3ea84e7171f5d5b2d4
thiagosalome <thiagosalome@gmail.com>
2018-04-22 13:23:30 -0300

    Consertando erros de merge no pacienteModel

--- app/models/paciente/pacienteModel.php
+++ app/models/paciente/pacienteModel.php
@@ -20,51 +20,20 @@ class pacienteModel{
             return "O nome deve ter entre 2 e 40 caracteres.";
         }   
         else if(!preg_match('#^\(\d{2}\) (9|)[6789]\d{3}-\d{4}$#', $pacienteVo->getTelefone())){
-<<<<<<< HEAD
-            return "not_an_mobile_number";
-=======
-            return "O número de telefone está inválido";//formato:(21) 98765-4321
->>>>>>> f564f422cc1bf7fcb4d0c3afc35894f729fb3b40
+            return "O número de telefone está inválido";
         }         
         else if(!preg_match("/^[a-z0-9\\.\\-\\_]+@[a-z0-9\\.\\-\\_]*[a-z0-9\\.\\-\\_]+\\.[a-z]{2,4}$/", $pacienteVo->getEmail())){
             return "O email digitado é inválido.";
         }
-<<<<<<< HEAD
         else if(!checkdate( $data[1] , $data[2] , $data[0] ) || $data[0] < 1900 || mktime( 0, 0, 0, $data[1], $data[2], $data[0] ) > time()){
-            return "invalid_date"; 
+            return "A data digitada é inválida."; 
         }
         else{
             $pacienteDAO = new pacienteDAO();                       
             $paciente = $pacienteDAO->getByCPF($pacienteVo->getCPF());            
            
             if(is_object($paciente)){
-=======
-        else if(!checkdate( $data[1] , $data[0] , $data[2] ) || $data[2] < 1900 || mktime( 0, 0, 0, $data[1], $data[0], $data[2] ) > time()){
-            return "A data de nascimento é inválida."; 
-        }
-        else{
-            
-            $pacienteDao = new pacienteDAO();
-            $cadastro = $pacienteDao->insert($pacienteVo);
-
-            // Verificar se usuário já existe
-            if(is_object($cadastro)){
-                return "O paciente atual já foi cadastrado.";
-            }
-
-
-
-
-
-/*
-            //add cpf no bd e pesquisar por ele
-                                   
-            $user = $pacienteDao->getByEmail($pacienteVo->getEmail());            
-            
-            // Verifica se usuário já existe
-            if(is_object($user)){
->>>>>>> f564f422cc1bf7fcb4d0c3afc35894f729fb3b40
-                return "already_registered";
+                return "O paciente já foi cadastrado antes.";
             }
             else if($paciente == null){
                 $cadastro = $pacienteDAO->insert($pacienteVo);
