95eb88094754e263a1f8b68af35ed3c6b4915e1e
thiagosalome <thiagosalome@gmail.com>
2018-05-16 11:20:18 -0300

    Separando o arquivo de estatísticas

--- app/views/paciente/adicionarPaciente.php
+++ app/views/paciente/adicionarPaciente.php
@@ -53,33 +53,7 @@
                 </div>
             </header>
             <div class="dashboard-statistics">
-                <div class="statistics-item">
-                    <div class="statistics-item-image-blue">
-                        <img src="<?php echo HOME_URI; ?>app/public/images/dashboard/patient_icon.png" alt="Pacientes" title="Pacientes" class="person-blue">
-                    </div>
-                    <div class="statistics-item-description">
-                        <span>192</span>
-                        <p>Pacientes</p>
-                    </div>
-                </div>
-                <div class="statistics-item">
-                    <div class="statistics-item-image-agua">
-                        <img src="<?php echo HOME_URI; ?>app/public/images/dashboard/aliments_icon.png" alt="Alimentos" title="Alimentos" class="person-blue">
-                    </div>
-                    <div class="statistics-item-description">
-                        <span>55</span>
-                        <p>Alimentos</p>
-                    </div>
-                </div>
-                <div class="statistics-item">
-                    <div class="statistics-item-image-green">
-                        <img src="<?php echo HOME_URI; ?>app/public/images/dashboard/diets_icon.png" alt="Dietas" title="Dietas" class="person-blue">
-                    </div>
-                    <div class="statistics-item-description">
-                        <span>32</span>
-                        <p>Dietas</p>
-                    </div>
-                </div>
+                <?php include __DIR__ . "/../objects/statistics.php"; ?>
             </div>
             <div class="dashboard-form">
                 <form role="form" class="largewidth js-form-addPatient" action="<?php echo HOME_URI; ?>paciente/create">
