bd24a597c50779ec69d0c1dce2d41ab477dc50d3
Lorran Martins <33589594+lorransm@users.noreply.github.com>
2018-06-10 18:13:31 -0300

    Aplicando alterações no banco. Subindo Diagrama ER.

--- app/database/db_nutrion.sql
+++ app/database/db_nutrion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 10-Jun-2018 às 20:15
+-- Generation Time: 10-Jun-2018 às 23:02
 -- Versão do servidor: 5.7.21-log
 -- PHP Version: 7.2.2
 
@@ -31,23 +31,24 @@ SET time_zone = "+00:00";
 CREATE TABLE `tb_alimento` (
   `id` int(11) NOT NULL,
   `nome` char(40) NOT NULL,
-  `medida` varchar(50) NOT NULL,
-  `tipoProteina` varchar(50) NOT NULL,
+  `medida` char(40) NOT NULL,
+  `tipoProteina` char(15) NOT NULL,
   `proteina` float NOT NULL,
+  `caloria` float NOT NULL,
   `carboidrato` float NOT NULL,
   `gordura` float NOT NULL,
-  `caloria` float NOT NULL
+  `id_nutricionista` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `tb_alimento`
 --
 
-INSERT INTO `tb_alimento` (`id`, `nome`, `medida`, `tipoProteina`, `proteina`, `carboidrato`, `gordura`, `caloria`) VALUES
-(1, 'batata', '', '', 4, 20, 0, 24),
-(2, 'ervilha', '', '', 3, 3, 0, 6),
-(3, 'peixe', '', '', 20, 20, 8, 50),
-(4, 'frango', '', '', 25, 10, 4, 39);
+INSERT INTO `tb_alimento` (`id`, `nome`, `medida`, `tipoProteina`, `proteina`, `caloria`, `carboidrato`, `gordura`, `id_nutricionista`) VALUES
+(1, 'batata', 'grama', 'vegetal', 2, 4, 2, 0, 1),
+(2, 'ervilha', 'grama', 'vegetal', 1, 2, 1, 0, 1),
+(3, 'peixe', 'grama', 'animal', 3, 7, 2, 2, 1),
+(4, 'frango', 'grama', 'animal', 4, 11, 3, 4, 1);
 
 -- --------------------------------------------------------
 
@@ -137,9 +138,9 @@ CREATE TABLE `tb_itemrefeicao` (
 
 INSERT INTO `tb_itemrefeicao` (`id_refeicao`, `id_alimento`, `quantidade`) VALUES
 (1, 2, 4),
+(2, 2, 1),
 (2, 4, 1),
-(3, 3, 1),
-(3, 4, 1);
+(3, 3, 1);
 
 -- --------------------------------------------------------
 
@@ -159,9 +160,9 @@ CREATE TABLE `tb_nutricionista` (
 --
 
 INSERT INTO `tb_nutricionista` (`id`, `email`, `senha`, `nome`) VALUES
-(1, 'cjunqueira@nutrion.com', '12345', 'Carlos Junqueira'),
-(2, 'jtavares@nutrion.com', 'abcde', 'Juliana Tavares'),
-(3, 'admin@admin.com', 'admin', 'Administrador'),
+(1, 'admin@admin.com', 'admin', 'Administrador'),
+(2, 'cjunqueira@nutrion.com', '12345', 'Carlos Junqueira'),
+(3, 'jtavares@nutrion.com', 'abcde', 'Juliana Tavares'),
 (4, 'teste@teste.com', 'teste', 'testeuser');
 
 -- --------------------------------------------------------
@@ -176,7 +177,7 @@ CREATE TABLE `tb_paciente` (
   `telefone` char(15) NOT NULL,
   `sexo` char(1) NOT NULL,
   `cpf` char(15) NOT NULL,
-  `email` varchar(100) DEFAULT NULL,
+  `email` char(40) DEFAULT NULL,
   `dataNasc` date NOT NULL,
   `id_nutricionista` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -186,10 +187,9 @@ CREATE TABLE `tb_paciente` (
 --
 
 INSERT INTO `tb_paciente` (`id`, `nome`, `telefone`, `sexo`, `cpf`, `email`, `dataNasc`, `id_nutricionista`) VALUES
-(1, 'Everaldo Dias', '(31)99999-9999', 'M', '999.999.999-99', NULL, '1996-04-30', 1),
-(2, 'Mariana Souza', '(31)3030-4444', 'F', '111.111.111-11', NULL, '1976-03-28', 2),
-(3, 'Douglas Miranda', '(31)3343-2244', 'M', '222.222.222-22', NULL, '1985-02-11', 3),
-(4, 'Tek', '(31)99659-9999', 'F', '850.224.880-43', 'tekuxuzeze@mailinator.com', '2013-03-16', 1);
+(1, 'Everaldo Dias', '(31)99999-9999', 'M', '999.999.999-99', 'everaldo@gmail.com', '1996-04-30', 1),
+(2, 'Mariana Souza', '(31)3030-4444', 'F', '111.111.111-11', 'mariana@gmail.com', '1976-03-28', 1),
+(3, 'Douglas Miranda', '(31)3343-2244', 'M', '222.222.222-22', 'douglas@gmail.com', '1985-02-11', 1);
 
 -- --------------------------------------------------------
 
@@ -210,8 +210,8 @@ CREATE TABLE `tb_refeicao` (
 
 INSERT INTO `tb_refeicao` (`id`, `nome`, `horario`, `id_dieta`) VALUES
 (1, 'café da manhã', '09h00', 1),
-(2, 'almoço', '12h00', 2),
-(3, 'almoço', '12h00', 1);
+(2, 'almoço', '12h00', 1),
+(3, 'janta', '19h00', 2);
 
 --
 -- Indexes for dumped tables
@@ -221,28 +221,29 @@ INSERT INTO `tb_refeicao` (`id`, `nome`, `horario`, `id_dieta`) VALUES
 -- Indexes for table `tb_alimento`
 --
 ALTER TABLE `tb_alimento`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_nutricionistaXalimento` (`id_nutricionista`);
 
 --
 -- Indexes for table `tb_avaliacao`
 --
 ALTER TABLE `tb_avaliacao`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_id_infoFisicas` (`id_infoFisicas`);
+  ADD KEY `fk_infofisicasXavaliacao` (`id_infoFisicas`);
 
 --
 -- Indexes for table `tb_dieta`
 --
 ALTER TABLE `tb_dieta`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_id_dietapaciente` (`id_paciente`);
+  ADD KEY `fk_pacienteXdieta` (`id_paciente`);
 
 --
 -- Indexes for table `tb_infofisicas`
 --
 ALTER TABLE `tb_infofisicas`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_id_paciente` (`id_paciente`);
+  ADD KEY `fk_pacienteXinfofisicas` (`id_paciente`);
 
 --
 -- Indexes for table `tb_itemrefeicao`
@@ -263,14 +264,14 @@ ALTER TABLE `tb_nutricionista`
 ALTER TABLE `tb_paciente`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `cpf` (`cpf`),
-  ADD KEY `fk_id_nutricionista` (`id_nutricionista`);
+  ADD KEY `fk_nutricionistaXpaciente` (`id_nutricionista`);
 
 --
 -- Indexes for table `tb_refeicao`
 --
 ALTER TABLE `tb_refeicao`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_id_dieta` (`id_dieta`);
+  ADD KEY `fk_dietaXrefeicao` (`id_dieta`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -310,7 +311,7 @@ ALTER TABLE `tb_nutricionista`
 -- AUTO_INCREMENT for table `tb_paciente`
 --
 ALTER TABLE `tb_paciente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tb_refeicao`
@@ -323,34 +324,40 @@ ALTER TABLE `tb_refeicao`
 --
 
 --
+-- Limitadores para a tabela `tb_alimento`
+--
+ALTER TABLE `tb_alimento`
+  ADD CONSTRAINT `fk_nutricionistaXalimento` FOREIGN KEY (`id_nutricionista`) REFERENCES `tb_nutricionista` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Limitadores para a tabela `tb_avaliacao`
 --
 ALTER TABLE `tb_avaliacao`
-  ADD CONSTRAINT `fk_id_infoFisicas` FOREIGN KEY (`id_infoFisicas`) REFERENCES `tb_infofisicas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_infofisicasXavaliacao` FOREIGN KEY (`id_infoFisicas`) REFERENCES `tb_infofisicas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limitadores para a tabela `tb_dieta`
 --
 ALTER TABLE `tb_dieta`
-  ADD CONSTRAINT `fk_id_dietapaciente` FOREIGN KEY (`id_paciente`) REFERENCES `tb_paciente` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_pacienteXdieta` FOREIGN KEY (`id_paciente`) REFERENCES `tb_paciente` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limitadores para a tabela `tb_infofisicas`
 --
 ALTER TABLE `tb_infofisicas`
-  ADD CONSTRAINT `fk_id_paciente` FOREIGN KEY (`id_paciente`) REFERENCES `tb_paciente` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_pacienteXinfofisicas` FOREIGN KEY (`id_paciente`) REFERENCES `tb_paciente` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limitadores para a tabela `tb_paciente`
 --
 ALTER TABLE `tb_paciente`
-  ADD CONSTRAINT `fk_id_nutricionista` FOREIGN KEY (`id_nutricionista`) REFERENCES `tb_nutricionista` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_nutricionistaXpaciente` FOREIGN KEY (`id_nutricionista`) REFERENCES `tb_nutricionista` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limitadores para a tabela `tb_refeicao`
 --
 ALTER TABLE `tb_refeicao`
-  ADD CONSTRAINT `fk_id_dieta` FOREIGN KEY (`id_dieta`) REFERENCES `tb_dieta` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_dietaXrefeicao` FOREIGN KEY (`id_dieta`) REFERENCES `tb_dieta` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
