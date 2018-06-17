3f746d320374ccd4606d882809469794a52a1297
Lorran Martins <33589594+lorransm@users.noreply.github.com>
2018-06-17 18:24:54 -0300

    Subindo refeicao controller, model, Vo e DAO

--- app/models/refeicao/refeicaoDAO.php
+++ app/models/refeicao/refeicaoDAO.php
@@ -1,5 +1,54 @@
 <?php
 class refeicaoDAO{
+    public function getAll($idDieta){
+        require "app/bootstrap.php";
+        $dieta = $entityManager->find("Dieta", $idDieta);
+        $refeicoes = $dieta->getRefeicoes();
+        return $refeicoes;
+    }
 
+    public function getById($idRefeicao){
+        require "app/bootstrap.php";
+        $refeicao = $entityManager->find("Refeicao", $idRefeicao);
+        return $refeicao;
+    }
+
+
+    public function create(refeicaoVo $refeicaoVo){
+        require "app/bootstrap.php";        
+        $refeicao = new Refeicao;
+        $dieta = $entityManager->find("Dieta", $refeicaoVo->getDieta());
+
+        $refeicao->setNome($refeicaoVo->getNome());
+        $refeicao->setHorario($refeicaoVo->getHorario());
+        $refeicao->setDieta($dieta);
+            
+        $entityManager->persist($refeicao);           
+        $entityManager->flush();
+        return $refeicao;
+    }
+
+    public function update(refeicaoVo $refeicaoVo){
+        require "app/bootstrap.php";
+        $update = $entityManager->find('Refeicao', $refeicaoVo->getId());
+
+        $update->setNome($refeicaoVo->getNome());
+        $update->setHorario($refeicaoVo->getHorario());
+        $update->setDieta($refeicaoVo->getDieta());
+
+        $entityManager->persist($update); 
+        $entityManager->flush();
+
+        return $update;
+    }
+
+    public function delete(refeicaoVo $refeicaoVo){
+        require "app/bootstrap.php";
+
+        $delete = $entityManager->find('Refeicao', $refeicaoVo->getId());
+        $entityManager->remove($delete); 
+        $entityManager->flush();
+        return true;
+    }
 }
 ?>
--- app/models/refeicao/refeicaoVo.php
+++ app/models/refeicao/refeicaoVo.php
@@ -1,5 +1,54 @@
 <?php
 class refeicaoVo{
+    private $id;
+    private $nome; 
+    private $horario;
+    private $dieta;
+     
+    public function getId()
+    {
+        return $this->id;
+    }
     
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getHorario()
+    {
+        return $this->horario;
+    }
+    
+    public function setHorario($horario)
+    {
+        $this->horario = $horario;
+        return $this;
+    }
+
+    public function getDieta()
+    {
+        return $this->dieta;
+    }
+
+    public function setDieta($dieta)
+    {
+        $this->dieta = $dieta;
+        return $this;
+    }
 }
 ?>
